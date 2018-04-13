1d9cff4d4327887f382db9a6a20a13412d1512d2
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-04-13 14:26:09 +0300

    Update queue.cpp

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -6,28 +6,48 @@
 TEST_CASE("creating_tree")
 {
 	queue_t<int> q;
-	REQUIRE( q.head() == nullptr );
-	REQUIRE( q.tail() == nullptr );
+	REQUIRE( q.head == nullptr );
+	REQUIRE( q.tail == nullptr );
 }
 
 TEST_CASE("push and pop int")
 {
 	queue_t<int> q;
 	q.push(3);
+	REQUIRE( q.head == 3 );
+	REQUIRE( q.tail == 3 );
+	
 	q.push(6);
-	int sum = q.pop();
-	sum = sum + q.pop();
-	REQUIRE( sum == 9 );
+	REQUIRE( q.head == 3 );
+	REQUIRE( q.tail == 6 );
+	
+	q.push(9);
+	REQUIRE( q.head == 3 );
+	REQUIRE( q.tail == 9 );
+	
+	REQUIRE( q.pop() == 3 );
+	REQUIRE( q.pop() == 6 );
+	REQUIRE( q.pop() == 9 );
 }
 
 TEST_CASE("push and pop double")
 {
 	queue_t<double> q;
 	q.push(3.3);
+	REQUIRE( q.head == 3.3 );
+	REQUIRE( q.tail == 3.3 );
+	
 	q.push(6.6);
-	double sum = q.pop();
-	sum = sum + q.pop();
-	REQUIRE( sum == 9.9 );
+	REQUIRE( q.head == 3.3 );
+	REQUIRE( q.tail == 6.6 );
+	
+	q.push(9.9);
+	REQUIRE( q.head == 3.3 );
+	REQUIRE( q.tail == 9.9 );
+	
+	REQUIRE( q.pop() == 3.3 );
+	REQUIRE( q.pop() == 6.6 );
+	REQUIRE( q.pop() == 9.9 );
 }
 
 TEST_CASE("throw")
@@ -41,9 +61,25 @@ TEST_CASE("copy")
 	queue_t<int> q1;
 	q1.push(3);
 	q1.push(6);
+	q1.push(9);
 	queue_t<int> q2(q1);
 	int sum = q2.pop();
 	sum = sum + q2.pop();
-	REQUIRE( sum == 9 );
+	sum = sum + q2.pop();
+	REQUIRE( sum == 18 );
+	
+}
+
+TEST_CASE("copy =")
+{
+	queue_t<int> q1;
+	q1.push(3);
+	q1.push(6);
+	q1.push(9);
+	queue_t<int> q2 = q1;
+	int sum = q2.pop();
+	sum = sum + q2.pop();
+	sum = sum + q2.pop();
+	REQUIRE( sum == 18 );
 	
 }
